cb4de564c949bace86d5e1ce0438c1a29ae84dab
jabir254 <jabirkullow@gmail.com>
2023-06-21 17:01:06 +0300

    created teacher model

--- school/app/models.py
+++ school/app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class student(models.Model):
     student_id = models.AutoField(primary_key = True)
+    password = models.CharField(max_length=10)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
@@ -19,3 +20,18 @@ class student(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+    
+class teacher(models.model):
+    teacher_id = models.AutoField(primary_key = True)
+    password = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    date_joined_school = models.DateField()
+    SEX_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
