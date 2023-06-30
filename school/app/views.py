f992c4e0582632f454878f6027fd9652e2a9ab62
jabir254 <jabirkullow@gmail.com>
2023-06-30 19:21:23 +0300

    updating login form

--- school/app/views.py
+++ school/app/views.py
@@ -4,7 +4,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
-
-
-def login(request):
-    return render(request,'core/login.html')
